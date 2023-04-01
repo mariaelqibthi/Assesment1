5eb9e66a81b0b104bc55ce319de9f0c71e701470
Maria El Qibthiya Az Zahra <itsbaymaxel@gmail.com>
2023-04-01 21:29:31 +0700

    initial commit progress

---
+++ app/src/main/java/setting/settingActivity.kt
@@ -0,0 +1,12 @@
+package setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.d3if3137.musicplayer.R
+
+class settingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
